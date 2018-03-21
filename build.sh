ce58ada989fe7424a1ed8fbaed523c83d7c98ddf
MagicMarvMan <24806960+MagicMarvMan@users.noreply.github.com>
2018-03-21 12:06:51 +0100

    Improvements

---
+++ build.sh
@@ -0,0 +1,3 @@
+grunt
+grunt bundle --themes=modern --plugins=advlist,anchor,autolink,autoresize,autosave,charmap,code,codesample,colorpicker,contextmenu,directionality,emoticons,fullpage,fullscreen,help,hr,image,imagetools,insertdatetime,link,lists,media,pagebreak,paste,preview,print,save,searchreplace,spellchecker,tabfocus,table,textcolor,visualblocks,visualchars,wordcount
+
